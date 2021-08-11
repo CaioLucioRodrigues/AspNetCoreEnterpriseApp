c7976386eb26f729f457abb1b5132949b97e0114
Caio Rodrigues <caio.rodrigues@ecalc.com.br>
2021-08-11 00:43:26 -0300

    Starting Client API

---
+++ src/services/NSE.Cliente.API/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+        //EF Relation
+        protected Cliente() { }
+
+        public Cliente(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Cpf { get; private set; }
+
+        public bool Excluido { get; private set; }
+
+        public Endereco Endereco { get; private set; }
+    }
+}
